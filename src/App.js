3748dfb3b47f90d12284f1763be5fba41f2c0ad8
yuddomack <darr880406@gmail.com>
2020-06-14 14:27:49 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useEffect } from "react";
+import d2i from "dom-to-image";
 
 function App() {
+  const targetDom = useRef(null);
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    d2i.toPng(targetDom.current).then((url) => setImageUrl(url));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div ref={targetDom}>
+        <h1>이 글자는 테스트 용 글자입니다</h1>
+      </div>
+      {imageUrl && <img src={imageUrl} alt="lyric" />}
     </div>
   );
 }
